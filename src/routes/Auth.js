4b4335e80eadd6f4f889f52545675c7ad7dd50bb
chacha912 <dkwkdnwk912@gmail.com>
2022-09-13 17:12:32 +0900

    Add login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,36 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from 'react';
 
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name='email' type='email' placeholder='Email' value={email} onChange={onChange} required />
+        <input name='password' type='password' placeholder='Password' value={password} onChange={onChange} required />
+        <button type='submit'>Log In</button>
+      </form>
+      <div>
+        <button type='button'>Continue with Google</button>
+        <button type='button'>Continue with Github</button>SD
+      </div>
+    </div>
+  );
+};
 export default Auth;
