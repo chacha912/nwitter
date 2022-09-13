61139c52e954d303764322bf0d42a7a4de011937
chacha912 <dkwkdnwk912@gmail.com>
2022-09-13 16:53:39 +0900

    라우터 수정 & 절대경로 설정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Auth, Home } from '../routes';
+import { Auth, Home } from 'routes';
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Routes>{isLoggedIn ? <Route path='/' element={<Home />} /> : <Route path='/' element={<Auth />} />}</Routes>
