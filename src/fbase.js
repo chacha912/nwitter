c4997b131485bf99e234db2e669dfd976a450e93
chacha912 <dkwkdnwk912@gmail.com>
2022-09-13 19:01:01 +0900

    Add nweets addDoc, getDocs in firestore

--- src/fbase.js
+++ src/fbase.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import {
+  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged as FonAuthStateChanged,
 } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,7 +22,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const authService = getAuth();
@@ -35,3 +36,7 @@ export const onAuthStateChanged = (fn) => {
   FonAuthStateChanged(authService, fn);
 };
 export { GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
+
+// Initialize Cloud Firestore and get a reference to the service
+export const dbService = getFirestore(app);
+export { collection, addDoc, getDocs } from 'firebase/firestore';
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,58 @@
-const Home = () => <span>Home</span>;
+import { useEffect, useState } from 'react';
+import { dbService, collection, addDoc, getDocs } from 'fbase';
+
+const Home = () => {
+  const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(dbService, 'nweets'), {
+        text: nweet,
+        createdAt: Date.now(),
+      });
+      setNweet('');
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  const getNweets = async () => {
+    const querySnapshot = await getDocs(collection(dbService, 'nweets'));
+    querySnapshot.forEach((doc) => {
+      const nweetObject = { ...doc.data(), id: doc.id };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type='text' placeholder="What's on your mind?" maxLength={120} />
+        <button type='submit'>Nweet</button>
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h2>{nweet.text}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
